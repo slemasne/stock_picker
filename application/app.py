2711bb7307595c586277be2f99fc25d0b8dc4b87
slemasne <stephenlemasney@gmail.com>
2018-07-31 00:30:36 +0100

    updates

--- application/app.py
+++ application/app.py
@@ -18,8 +18,8 @@ def results():
         result_count = request.form.get("number")
         print(result_count)
         print (request.form)
-        df = stockSelector(2, columns).stock_stats()
-        return render_template('results.html', risk = risk, market=market, table = df.to_html(classes="table-striped"))
+        selector_results = stockSelector(int(result_count), columns).stock_stats()
+        return render_template('results.html', risk = risk, market=market, table = selector_results.to_html(classes="table-striped"))
     except KeyError:
         print("Page has failed - please go back")
 
